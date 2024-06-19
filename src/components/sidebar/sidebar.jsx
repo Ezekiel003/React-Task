58e2efbd5e911505bdf3cfa930c9f3f4f298096e
Ezekiel <bussyyoni400@gmail.com>
2024-06-19 15:56:28 +0100

    added img

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -4,6 +4,10 @@ import logo from "/arcticons_google-messages.svg";
 import logo2 from "/Scrutz.svg";
 import { TbBulb } from "react-icons/tb";
 import logo1 from"/ri_dashboard-2-line.svg"
+import settings from "/ep_setting.svg"
+import intelligence from "/fluent-mdl2_insights.svg"
+import campaign from "/material-symbols_campaign-outline.svg"
+import help from "/material-symbols_help-outline.svg"
 const SideBar = () => {
   return (
     <div className="sidebar">
@@ -20,19 +24,19 @@ const SideBar = () => {
             <img src={logo1} alt="" /> Overview
           </NavLink>
           <NavLink to="Campaign" className="nav-links">
-            <img src="/material-symbols_campaign-outline.svg" alt="" />Campaign
+            <img src={campaign} alt="" />Campaign
           </NavLink>
           <NavLink to="market-intelligence" className="nav-links">
-            <img src="/fluent-mdl2_insights.svg" alt="" />
+            <img src={intelligence} alt="" />
             Market intelligence
           </NavLink>
           <NavLink to="settings" className="nav-links">
-            <img src="/ep_setting.svg" alt="" />
+            <img src={settings} alt="" />
             Account Settings
           </NavLink>
         </div>
         <div className="help">
-            <img src="/material-symbols_help-outline.svg" alt="" />
+            <img src={help} alt="" />
             <div className="title">Need Help?</div>
             <p className="small-text">We are readily available <br /> to provide help</p>
             <button className="outline-btn">Get Help</button>
